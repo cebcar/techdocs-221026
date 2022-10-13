4a83878d27149638afeec74c39569d55d99c1bbd
Carol Clark <3742721+carolclark@users.noreply.github.com>
2022-10-13 17:09:03 -0500

    aliases: add ldt, sorted log sample with color
    ldt saved here primarily as an example of correct color handleing

--- aliases.zsh
+++ aliases.zsh
@@ -5,3 +5,4 @@ alias gwb='mkdir -p .cebcar; branchlist=".cebcar/branchlist"; git branch | grep 
 alias gsb='autoload -U colors && colors;echo "$fg[yellow]\033[1m";pwd;echo "$reset_color";git status;echo "\nall branches:";git branch;echo "    wb: $fg[cyan]$wb$reset_color\n";echo "unmerged branches:\n";git branch --no-merged'
 alias glgr='git log --oneline --graph'
 alias glog='git log --oneline --graph -6'
+alias ldt='git log --graph --decorate -30 --all --date-order --date=format:"%Y-%m-%d %H:%M:%S" --pretty=format:"%C(cyan)%h%Creset %C(black bold)%ad%Creset%C(auto)%d %s"'
